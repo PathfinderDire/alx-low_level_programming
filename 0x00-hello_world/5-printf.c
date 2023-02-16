8f3686d88c611c03d5ef87ed2b8c8ac8ef3f152d
Laelaf <laelaf.sirak@gmail.com>
2023-02-16 15:36:41 -0800

    kjsdf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * Main - Entry Point
+ * main - Entry Point
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,/n");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 
 
