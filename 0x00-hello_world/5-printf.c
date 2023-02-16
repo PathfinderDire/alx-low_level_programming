623c614ca862598864b2dbf606248a966b09adca
Laelaf <laelaf.sirak@gmail.com>
2023-02-16 15:41:34 -0800

    aksjd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
+ * main - entry point
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,") ;
-	return (0);
+        puts("with proper grammar, but the outcome is a piece of art,");
+        return (0);
 
 
 }
