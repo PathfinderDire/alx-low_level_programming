a052193f52042eb31daba9301fb2dab60fda9606
Laelaf <laelaf.sirak@gmail.com>
2023-02-20 11:54:16 -0800

    jkdh

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,9 +10,13 @@ int main(void)
 {
 	int n = 97;
 
-	while ((n <= 122) || ((n != 101) && (n != 113)))
-	{putchar(n);
-		n++;
+	while (n <= 122)
+	{
+		if (n != 101 && n != 113)
+		{
+			putchar(n);
+			n++;
+		}
 	};
 	putchar('\n');
 	return (0);
