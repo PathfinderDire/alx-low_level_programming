f47c72b56d4323956b0ab1f182205780de913a0a
Laelaf <laelaf.sirak@gmail.com>
2023-02-20 11:24:38 -0800

    sdlkf

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - beginig of Code
+ *
+ * Return:is Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	while ((n <= 122) && (n != 113 && n != 145))
+	{putchar(n);
+		n++;
+	};
+	putchar('\n');
+	return (0);
+
+
+}
