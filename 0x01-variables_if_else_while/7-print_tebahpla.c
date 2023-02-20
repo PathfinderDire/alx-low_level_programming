131c7ca66c4cf5255cea0b6106aeb125f04cb54a
Laelaf <laelaf.sirak@gmail.com>
2023-02-20 12:40:04 -0800

    asdkh

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - beginig of Code
+ *
+ * Return:is Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{putchar(n);
+		n--;
+	};
+	putchar('\n');
+	return (0);
+
+
+}
