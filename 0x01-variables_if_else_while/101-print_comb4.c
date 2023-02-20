6528e68af47c94fd45b338bd2425136fb5730059
Laelaf <laelaf.sirak@gmail.com>
2023-02-20 14:28:45 -0800

    sdfklj

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - beginig of Code
+ *
+ * Return:is Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{putchar(n);
+		n++;
+	};
+	putchar('\n');
+	return (0);
+
+
+}
