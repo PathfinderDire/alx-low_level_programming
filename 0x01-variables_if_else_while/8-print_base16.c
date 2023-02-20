0919a9c1263ef7a7d57fb2c84777da844f1a251f
Laelaf <laelaf.sirak@gmail.com>
2023-02-20 13:27:34 -0800

    skjdh

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main: beginning of code 
+ *
+ * Return:is always 0 (Success)
+ */
+
+int main(void)
+{
+	int n=0;
+	char letter ='a';
+
+	if (n <= 10)
+	{ putchar(n);
+		n++;
+	};
+	if (letter <= 'f')
+	{putchar(letter);
+		letter++;
+	};
+	return (0);
+
+
+}
