0b4bd820e54ebe1de87973f5ad4c5ac2ddacc24c
Laelaf <laelaf.sirak@gmail.com>
2023-02-20 03:18:14 -0800

    asjldb

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - Begining of Code
  *
- * return: Always 0(Success)
+ * Return:is Always 0 (Success)
  */
 
 int main(void)
@@ -15,13 +15,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		{ printf("%d" ,n) ; 
+		{ printf("%d", n);
 			printf(" is positive\n") ; }
 	else if (n == 0)
 	{ printf("%d", n);
 		printf(" is zero\n") ; }
-		else 
-		{ printf("%d", n) ; 
-			printf(" is negative\n") ; };
+		else
+		{ printf("%d", n);
+			printf(" is negative\n")
+		};
 	return (0);
 }
