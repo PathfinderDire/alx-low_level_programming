c1a7830dcf2d7f409640f64b11b5f7a99c5a5266
Laelaf <laelaf.sirak@gmail.com>
2023-02-20 02:04:59 -0800

    skdjfh

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,9 +15,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n>0)
-		{ printf("%d",n); printf("\nis positive\n");}
+		{ printf("%d",n); printf(" is positive\n");}
 	if (n==0)
-	{ printf("%d",n); printf("\nis zero\n");}
-		else {printf("%d",n); printf("\nis negative\n");};
+	{ printf("%d",n); printf(" is zero\n");}
+		else {printf("%d",n); printf(" is negative\n");};
 	return (0);
 }
