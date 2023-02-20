1aee24a4a2fa5cf2836b14485c2f20a6936f24f6
Laelaf <laelaf.sirak@gmail.com>
2023-02-20 10:06:58 -0800

    kjadshf

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - starting of code
+ *
+ * Return:is Always 0 (Success)
+ */
+
+int main (void)
+{
+	int n=97;
+	int p=65;
+	
+	while (n <= 122)
+	{ putchar(n);
+		n++;
+	};
+	while (p <= 65)
+	{ putchar(n);
+		n++;
+	};
+	putchar ('\n');
+	return (0);
+
+
+}
