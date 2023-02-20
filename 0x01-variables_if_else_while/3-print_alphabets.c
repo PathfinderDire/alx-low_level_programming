87a80f5a3b43a43fa5847e7e5dc75b2213e1b119
Laelaf <laelaf.sirak@gmail.com>
2023-02-20 10:56:47 -0800

    sdklfj

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,20 +6,20 @@
  * Return:is Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 	int n = 97;
-	int k = 65;
-	
-	while (n <= 122)
-	{ putchar('n');
-		n++;
-	};
+
+        while (n <= 122)
+        {putchar(n);
+                n++;
+        }
+        ;
 	while (k <= 90)
-	{ putchar('k');
-		n++;
+	{putchar(k);
+		k++;
 	};
-	putchar ('\n');
+	putchar('\n');
 	return (0);
 
 
