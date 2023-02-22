a2399f2ea7ba5325e76d5a8834c4d8384c9e45f1
Laelaf <laelaf.sirak@gmail.com>
2023-02-22 12:31:11 -0800

    alsdkh

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - prints alphabets 10 times
+ *
+ * Return:void
+ */
+
+void print_alphabet_x10(void)
+{
+	char n;
+	int i=0;
+
+	while(i < 10)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+        {
+                _putchar(n);
+        }
+		i++;
+	}
+        _putchar('\n');
+}
