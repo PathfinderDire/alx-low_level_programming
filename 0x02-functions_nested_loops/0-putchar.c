dbc50e648effa7c3e35d11b058c1d90ba330d94c
Laelaf <laelaf.sirak@gmail.com>
2023-02-22 09:00:53 -0800

    asdjklhl

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints out putchar
+ *
+ * Return:is Always 0
+ */
+
+int main (void)
+{
+	char msg[] = "_putchar";
+	int count = 0;
+	while(count < 8)
+	{ putchar(msg[count]);
+		count++;
+	};
+	putchar('\n');
+	return (0);
+
+
+}
