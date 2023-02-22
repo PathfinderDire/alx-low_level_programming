25e680811409411a27ae392396d68264ecf11b25
Laelaf <laelaf.sirak@gmail.com>
2023-02-22 14:58:32 -0800

    dsfh

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+ * int _abs - Prints Absolute value
+ * Return: is always posetiv no
+ */
+
+int _abs(int n)
+{
+	if (n<0)
+	{n = -1*n;
+	};
+	return (n);
+
+
+}
---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,15 @@
+#include "main.c"
+
+/**
+ * main - print abs val
+ * Return: Always zero 
+ * @n: number checked
+ */
+
+int main(void)
+{
+	int _abs();
+	return (0);
+
+
+}
