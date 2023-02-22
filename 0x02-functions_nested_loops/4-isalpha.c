ebd1c832cf6e061ddbc14888a652e96a37da01b6
Laelaf <laelaf.sirak@gmail.com>
2023-02-22 13:46:45 -0800

    dkfjshgk

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,14 +3,16 @@
 /**
  * _isalpha- Checks Alphabet
  *
- * Return: 1 if letter 0 if not 
+ * Return: 1 if letter 0 if not
  *
+ * c: is Char
+ * c - is char
  */
 
 int _isalpha(int c)
 {
 	if (((c >= 'a' && c <= 'z')) || ((c >= 'A' && c <= 'Z')))
-                return (1);
-        else
-                return (0);
+		return (1);
+	else
+		return (0);
 }
