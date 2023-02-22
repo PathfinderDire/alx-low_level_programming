e2926df1699ab4355add46c40cb28cf7ea4e1d13
Laelaf <laelaf.sirak@gmail.com>
2023-02-22 11:40:49 -0800

    asdjklha

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,2 @@
-#include <stdio.h>
-char _putchar(char c)
+char _putchar(char c);
 void print_alphabet(void)
-{
-        int n;
-
-        for (n = 'a'; n <= 'z'; n++)
-        {
-                putchar(n);
-        }
-        putchar('\n');
-
-
-}
