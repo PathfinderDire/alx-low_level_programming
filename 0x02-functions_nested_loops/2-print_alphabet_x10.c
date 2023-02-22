43ab5adc20735598ebc5052b89a56e1fb5683a40
Laelaf <laelaf.sirak@gmail.com>
2023-02-22 12:37:37 -0800

    kajsdk

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,22 +2,23 @@
 
 /**
  * main - prints alphabets 10 times
- *
+ * print_alphabet_x10 - Prints 10x
  * Return:void
  */
 
 void print_alphabet_x10(void)
 {
 	char n;
-	int i=0;
+	int i = 0;
 
-	while(i < 10)
+	while (i < 10)
 	{
 		for (n = 'a'; n <= 'z'; n++)
-        {
-                _putchar(n);
-        }
+		{
+			_putchar(n);
+		}
+		_putchar('\n');
 		i++;
 	}
-        _putchar('\n');
+	_putchar('\n');
 }
