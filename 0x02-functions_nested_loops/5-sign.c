42f2ca86b2613e909cc77b9a256b21376bfaed93
Laelaf <laelaf.sirak@gmail.com>
2023-02-22 14:29:33 -0800

    skdfj

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,16 +11,15 @@ int print_sign(int n)
 	if (n > 0)
 	{ _putchar('+');
 		return (1);
-	}
+	};
 	if (n == 0)
 	{ _putchar(n);
 		return (0);
-	}
+	};
 	if(n<0) 
 	{ _putchar('-');
 		return (-1);
 	};
-	return (0);
 
 
 }
