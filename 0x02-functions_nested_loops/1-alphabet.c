9308b13c2dd97252b88bc7f2815b790eac65934d
Laelaf <laelaf.sirak@gmail.com>
2023-02-22 09:39:57 -0800

    djksfhs

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Biginning of Code
+ * Return:on Success 0
+ */
+void print_alphabet(void);
+int main(void)
+{
+	void print_alphabet(void){
+		int n = 97;
+		while (n <= 122)
+		{ putchar(n);
+			n++;
+		};
+		putchar('\n');
+	}
+
+
+}
