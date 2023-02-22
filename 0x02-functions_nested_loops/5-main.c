b26b5afcc249c9d6927252cd3dc0f99ed11be559
Laelaf <laelaf.sirak@gmail.com>
2023-02-22 14:06:45 -0800

    sdfkjshd

---
+++ 0x02-functions_nested_loops/5-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Checks if Pos , neg of Zero given no
+ * Return: Always 0
+ */
+
+int main(void) 
+{
+	print_sign();
+	return (0);
+
+
+}
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * int print_sign - prints sign of number
+ * Return: 1 if pos, 0 if zero , -1 if Neg
+ * @n: Nummber to be checked
+ * /
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{ _putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{ _putchar(n);
+		return (0);
+	}
+	if(n<0) 
+	{ _putchar('-');
+		return (-1);
+	}
+
+
+}
