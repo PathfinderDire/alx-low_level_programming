77240d87fa40bfd3513942d296eca37e58827248
Laelaf <laelaf.sirak@gmail.com>
2023-02-23 11:13:21 -0800

    asda

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks if the Char is Upper or Lower case
+ *
+ * Return:0 if upper case , 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (65 <= c <= 90)
+	{ return(1);
+	}
+	return (0);
+
+
+}
---
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/** 
+ * main - Checks letter if its upper case
+ * Return:Always 0
+ */
+
+int main(void)
+{
+	int _isupper();
+	return(0);
+
+
+}
