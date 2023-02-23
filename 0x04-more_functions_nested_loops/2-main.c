abeaf3e14971091fd86f72dbaa4549ef64529de4
Laelaf <laelaf.sirak@gmail.com>
2023-02-23 12:37:54 -0800

    Prints the Multiple of 2 Nums

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/** main - Prints Multiple
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int mul();
+	return (0);
+
+
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Prints Multiple
+ * Return: Multiple
+ * @a: Num 1
+ * @b: Num 2
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+
+
+}
