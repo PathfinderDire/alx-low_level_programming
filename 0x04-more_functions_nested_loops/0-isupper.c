ea1be2a12ebde678cb2eb628576f9784b5fb8123
Laelaf <laelaf.sirak@gmail.com>
2023-02-23 11:49:07 -0800

    Checking Aplh

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,13 @@
 
 /**
  * _isupper - Checks if the Char is Upper or Lower case
- *
- * Return:0 if upper case , 0 otherwise
+ * @c: Alphabet checked
+ * Return: is 1 if upper case 0 otherwise
  */
 
 int _isupper(int c)
 {
-	if (c >= 'A'  && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return(1);
 	else
 		return (0);
