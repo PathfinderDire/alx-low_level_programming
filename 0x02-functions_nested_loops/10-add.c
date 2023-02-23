3a7129e9e5518d8e24f91e57f6a859bfd13596f6
Laelaf <laelaf.sirak@gmail.com>
2023-02-22 16:31:43 -0800

    asda

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add  -  adds 2 nums
+ *
+ * Return: is Always Sum
+ */
+
+int add(int a,int b)
+{
+	int sum;
+
+	int sum = a + b;
+	return (sum);
+
+
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main -  Begginnig 
+ *
+ * Return: Is aalways 0
+ */
+
+int main(void)
+{
+	int add();
+	return (0);
+
+
+}
+ 
