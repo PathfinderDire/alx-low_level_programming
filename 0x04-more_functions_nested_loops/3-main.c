b022c1f7b59700ae8743988f3775eec835ae29e1
Laelaf <laelaf.sirak@gmail.com>
2023-02-23 12:51:28 -0800

    Prints from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Prints 0 - 9
+ * Return : Always 0
+ */
+
+int main(void)
+{
+	void print_numbers();
+	return (0);
+
+
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints 0 -9
+ * Return: Always 0
+ * @n: Counter
+ */
+
+void print_numbers(void)
+{
+	int n = 48;
+
+	if (48 <= n && 57 >= n)
+	{
+		_putchar('n');
+		n++;
+	};
+
+
+}
