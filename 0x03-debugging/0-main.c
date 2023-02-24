18335648e506d1ef47bba49ff3f26a65b69a04cb
Laelaf <laelaf.sirak@gmail.com>
2023-02-24 04:21:59 -0800

    checks Posetive numbers

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
+}
+carrie@ubuntu:/debugging$
+carrie@ubuntu:/debugging$ cat main.h
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif 
