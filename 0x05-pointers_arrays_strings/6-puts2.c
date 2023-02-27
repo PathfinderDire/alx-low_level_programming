5e3e8918a7da075d65464c4d520ec8c3567ba7d7
Laelaf <laelaf.sirak@gmail.com>
2023-02-27 15:11:18 -0800

    sdfs

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,29 @@
-#
+#include "main.h"
+#include <string.h>
+#include <stddef.h>
+
+/**
+ * puts2 - Prints string;
+ * Return: returns Void;
+ * @str: string
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i + 2)
+	{
+		if (str[i] != ' ')
+		{
+			_putchar(str[i]);
+		}
+		else if (str[i] == ' ')
+		{
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+
+
+}
