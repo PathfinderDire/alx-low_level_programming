81084468a6194959438d164b80af4bf8d205e333
Laelaf <laelaf.sirak@gmail.com>
2023-02-27 10:26:54 -0800

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Array change
+ * Return: None
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+
+}
---
+++ 0x05-pointers_arrays_strings/1-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - changes pointer to 98
+ * retur: Alwasys 0
+ */
+
+int main(void)
+{
+	void reset_to_98();
+	return (0);
+
+
+}
