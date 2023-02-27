bceee6501175736d2b1b4422f1023c223c4dc8b4
Laelaf <laelaf.sirak@gmail.com>
2023-02-27 11:40:09 -0800

    Finds length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * _strlen - returns the length of string
+ * @s: Array
+ * return: lenth of string
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(*s);
+	return (length);
+
+
+}
---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/** main - Returns lenth of a string
+ * return: always 0
+ */
+
+int main(void)
+{
+	int _strlen();
+	return (0);
+
+
+}
