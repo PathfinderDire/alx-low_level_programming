c26fb76102b71ff7f28e2dc7be955b86e96d2128
Laelaf <laelaf.sirak@gmail.com>
2023-02-28 13:12:25 -0800

    asda

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 #include <string.h>
 #include <stddef.h>
@@ -15,7 +16,7 @@ void print_array(int *a, int n)
 	for (i = 0; i <= n; i++)
 	{
 		_putchar(a[i]);
-		_putchar(", ");
+		printf(", ");
 	};
 	_putchar('\n');
 
