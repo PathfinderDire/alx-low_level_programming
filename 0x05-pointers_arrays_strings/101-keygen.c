c820f1c504337055d1b95f79dacbde7bbedd4031
Laelaf <laelaf.sirak@gmail.com>
2023-02-28 14:43:16 -0800

    asda

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * print_rev - Prints string;
+ * Return: returns Void;
+ * @s: string
+ */
+
+void print_rev(char *s)
+{
+        int i;
+
+        for (i = strlen(s) - 1; i >= 0; i--)
+        {
+                if (s[i] != ' ')
+                {
+                        _putchar(s[i]);
+                }
+                else if (s[i] == ' ')
+                {
+                        _putchar(' ');
+                }
+        }
+        _putchar('\n');
+
+
+}
