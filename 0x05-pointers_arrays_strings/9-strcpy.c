6df49a63d41b46cf8c20cc620fdb2d5172354bd1
Laelaf <laelaf.sirak@gmail.com>
2023-02-28 13:52:44 -0800

    asda

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,9 +11,9 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-	
+	int p = strlen(src);
 	setbuf(stdout, dest);
-	for (i = 0; i <= strlen(src); i++)
+	for (i = 0; i <= p; i++)
 	{
 		if (src[i] != ' ')
 		{
