03a19a68bb38c7a746267864a2ff79a9284424f0
Laelaf <laelaf.sirak@gmail.com>
2023-03-02 12:24:44 -0800

    concatenate n byte only

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _strncat - Concatenates 2 Strings
+ * Return: the pointer Concatenated string
+ * @dest: Destinaton stg
+ * @src: Source String
+ * @n: Number of char to be concatenated
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	};
+	while (j <= n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	};
+	dest[i] = '\0';
+	return (dest);
+
+
+}
