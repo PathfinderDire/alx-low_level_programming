ed5b2eb7118ced54ed5e5bc5358c493e6f40f7b5
Laelaf <laelaf.sirak@gmail.com>
2023-03-02 15:52:59 -0800

    Compares two Stringess

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,16 +13,17 @@ int _strcmp(char *s1, char *s2)
 {
 	int p, i = 0;
 
-	while ((s1[i] == s2[i]) && (i < p))
+	p = strlen(s1);
+	while ((s1[i] == s2[i]) && (p < i))
 	{
 		i++;
 	};
-       if (s[i] < s2[i])
-	       return (s1[i] - s2[i]);
-       else if (s1[i] > s2[i])
-	       return (s[i] - s2[i]);
-       else
-	       return (0);
+	if (s1[i] == s2[i])
+		return (0);
+	else if (s1[i] > s2[i])
+		return (15);
+	else
+		return (-15);
 
 
 }
