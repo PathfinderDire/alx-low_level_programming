e98b90aa7c520a595a538dba8febb2b97b49dae7
Laelaf <laelaf.sirak@gmail.com>
2023-03-02 15:08:49 -0800

    Compares two Stringss

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -19,13 +19,13 @@ int _strcmp(char *s1, char *s2)
 		i++;
 	};
 	if (s1[i] == '\0')
-		return (-15);
+		return (-1);
 	else if (s1[i] == s2[i])
 		return (0);
 	else if (s1[i] > s2[i])
-		return (15);
+		return (1);
 	else
-		return (-15);
+		return (-1);
 
 
 }
