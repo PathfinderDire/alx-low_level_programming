72ce4b71dccea1b62bdc4d5ccc87ebd0005a6f6c
Laelaf <laelaf.sirak@gmail.com>
2023-03-03 14:25:23 -0800

    now handles null

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,21 +2,21 @@
 #include <string.h>
 
 /**
- * string_toupper- Changes all lower to upper case
+ * string_toupper - Changes all lower to upper case
  * Return: updated string pointer
- * @s: String accepted
+ * @str: string accepted
  */
 
-char *string_toupper(char *s)
+char *string_toupper(char *str)
 {
-	for (int i = 0; s[i] != '\0'; i++)
+	for (int i = 0; str[i] != '\0'; i++)
 	{
-		if ((s[i] >= 97) && (s[i] <= 122))
+		if ((str[i] >= 97) && (str[i] <= 122))
 		{
-			s[i] = s[i] - 32;
+			str[i] = str[i] - 32;
 		};
 	};
-	return (s);
+	return (str);
 
 
 }
