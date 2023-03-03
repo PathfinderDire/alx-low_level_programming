fc3aac3b71bdb777d025abedcd0ea5d73f6364b9
Laelaf <laelaf.sirak@gmail.com>
2023-03-03 13:12:14 -0800

    fixed reversing n ints

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,23 +4,29 @@
 #include <stdio.h>
 
 /**
- * reverse_array - reverses n intigers of string;
- * Return: returns Void;
- * @s: string
+ * reverse_array - reverses n intigers of string
+ * Return: returns Void
+ * @n: size of string
+ * @a: string to be revered
  */
 
 void reverse_array(int *a, int n)
 {
 	int i = 0, j = n - 1, temp;
 
-	while (i <= (n/2))
+	if (n != 2)
 	{
-		temp = a[i];
-		a[i] = a[j];
-		a[j] = temp;
-		i++;
-		j--;
-	};
+		while (i <= (n / 2))
+		{
+			temp = a[i];
+			a[i] = a[j];
+			a[j] = temp;
+			i++;
+			j--;
+		};
+	}
+	else
+		a[1] = a[2];
 
 
 }
