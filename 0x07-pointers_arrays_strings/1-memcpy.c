c084189da6d8481bb147fd416fc79c1d5092972f
Laelaf <laelaf.sirak@gmail.com>
2023-03-06 13:34:47 -0800

    asda

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -14,9 +14,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	int i = 0;
 	int j = 0;
 
-	while(n > 0)
+	while (n > 0)
 	{
-		dest[i] = src [j];
+		dest[i] = src[j];
 		i++;
 		j++;
 		n--;
