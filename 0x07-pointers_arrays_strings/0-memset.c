16391119278692bacbb3f99d9f16c4ff4ca35035
Laelaf <laelaf.sirak@gmail.com>
2023-03-06 12:26:02 -0800

    New pointer function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset - Sets n bytes with constant byte b
+ * @s: the pointer to memory to be assigned
+ * @b: constant byte
+ * @n: n bytess to be assigned with b
+ *
+ * Return: pointer to memory s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		s[i] = b;
+	};
+
+
+}
