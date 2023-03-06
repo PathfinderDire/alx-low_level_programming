34fe86fa9925240ae6896db5608ba3e126c14c74
Laelaf <laelaf.sirak@gmail.com>
2023-03-06 13:10:40 -0800

    New pointer function

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,12 +12,13 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	for (int i = 0; (i < n) && (n > 0); i++)
+	int i = 0;
+
+	while (n > 0)
 	{
 		s[i] = b;
+		i++;
 		n--;
-	};
+	}
 	return (s);
-
-
 }
