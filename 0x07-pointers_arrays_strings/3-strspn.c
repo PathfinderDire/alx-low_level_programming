f351880335abc0efa8aa27a0b0f3ff8539122236
Laelaf <laelaf.sirak@gmail.com>
2023-03-07 13:59:01 -0800

    update on char

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,36 +1,26 @@
 #include "main.h"
-#include <string.h>
-
 /**
- * _strspn - prints the number of bytes of src str in dest
- * @s: dest strimg
- * @accept: src string
- * Return: pointer to dest
+ * _strspn - function that gets the length of a prefix substring
+ *@s: first value -char
+ *@accept: second value - char
+ *
+ * Return: char with result
  */
-
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int cnt = 0;
+	int j = 0, i = 0;
+	unsigned int a = 0;
 
-	int lens = strlen(accept);
-	int lend = strlen(s);
-
-	for (int i = 0; i < lens; i++)
+	while (s[i] != ' ' && s[i] != '\0')
 	{
-		int true = 0;
-
-		for (int j = 0; j < lend; j++)
-		if (accept[j] == s[i])
+		while (accept[j] != '\0')
 		{
-			true = 1;
-			break;
-		};
-		if (true == 0)
-			break;
-		else
-			cnt++;
-	};
-	return (cnt);
-
-
+			if (s[i] == accept[j])
+				a++;
+			j++;
+		}
+		i++;
+		j = 0;
+	}
+	return (a);
 }
