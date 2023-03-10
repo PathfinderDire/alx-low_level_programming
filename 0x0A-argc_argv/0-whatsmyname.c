25d96220dad2cd37c00dd01ae874696b3e3d6603
Laelaf <laelaf.sirak@gmail.com>
2023-03-10 14:34:31 -0800

    prints name of program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-	printf("program's name is: %s\n",argv[0]);
-	return 0;
+/**
+ * main - prints name of prog
+ * @argv: argv
+ * @argc: argument number
+ * Return: void
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
 }
