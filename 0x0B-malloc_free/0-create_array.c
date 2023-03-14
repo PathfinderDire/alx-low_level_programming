b6d60098fee3aac0c03ff3993489f279d4a3b8ed
Laelaf <laelaf.sirak@gmail.com>
2023-03-14 14:58:15 -0700

    malloc allocation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,6 +13,7 @@ char *create_array(unsigned int size, char c)
 {
 	char *ptr = (char *) malloc(size);
 	unsigned int i = 0;
+
 	if (ptr == NULL)
 	{
 		printf("Can't allocate %d bytes (after %d calls)\n", size, i);
