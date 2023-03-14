677be929ccea90a6bd987e423736b333615a9e67
Laelaf <laelaf.sirak@gmail.com>
2023-03-14 13:45:07 -0700

    malloc allocation

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * create_array - creates an array of size 
+ * @size: size of me,eory allpc
+ * @c: character to be assigned
+ *
+ * Return: pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr = malloc(size);
+
+	if (size != 0)
+	{
+		for (int i = 0; i < size; i++)
+				ptr[i] = c;
+	}
+	else
+		return (NULL);
+}
