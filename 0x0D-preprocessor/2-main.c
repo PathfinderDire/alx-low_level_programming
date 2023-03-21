2be605bc983bee0b9fc9abbf0fbb4542e5cb7fb5
Laelaf <laelaf.sirak@gmail.com>
2023-03-21 12:12:18 -0700

    using Predefined macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints a program to print the File Name of program
+ *
+ * Return: Void
+ */
+void main(void)
+{
+	printf("%lu\n", __FILE__);
+}
