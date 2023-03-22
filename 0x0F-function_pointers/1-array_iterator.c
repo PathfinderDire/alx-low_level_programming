c547c617f9331b991172b73a52f9c26e8606b14a
Laelaf <laelaf.sirak@gmail.com>
2023-03-22 15:30:36 -0700

    itteration with array pointer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	if ((array == NULL) || (size == 0))
 		return;
-	for (int i = 0; i < size - 1; i++)
+	for (int i = 0; i < size; i++)
 	{
 		action(array[i]);
 	};
