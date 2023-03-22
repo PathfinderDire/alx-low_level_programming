08ff5b6ca4c8591ba06ef69f3053bdd39de0fc85
Laelaf <laelaf.sirak@gmail.com>
2023-03-22 16:10:57 -0700

    array index finder

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,18 +11,14 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	if (size > 0)
-	{
-		unsigned int i;
+	int i;
 
-		for (i = 0; i < size; i++)
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]) != 0)
-			{
 				return (i);
-			};
 		};
-	}
-	else
-		return (-1);
+	return (-1);
 }
