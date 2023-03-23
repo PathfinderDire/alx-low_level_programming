c331db467a97ec6586a12e3bc6a8b4a92a075232
Laelaf <laelaf.sirak@gmail.com>
2023-03-23 16:18:27 -0700

    variardic fun counter fix

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef VARIARDIC_FUNCTIONS_H
+#define VARIARDIC_FUNCTIONS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+int sum_them_all(const unsigned int n, ...);
+#endif /* VARIARDIC_FUNCTIONS_H */
