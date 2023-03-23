b4540bd91cc3e91c69f33e4eb3aad7c91e6955b8
Laelaf <laelaf.sirak@gmail.com>
2023-03-23 14:27:11 -0700

    function caller pointer fix

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,7 +19,7 @@ int (*get_op_func(char *s))(int a, int b)
     };
 	int i = 0;
 
-	while (ops[i].op != 'NULL' && *(ops[i].op) != *s)
-			i++
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+			i++;
 				return (ops[i].f);
 }
