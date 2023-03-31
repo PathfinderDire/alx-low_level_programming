248643c84c9b2fccbaae5bdbd04a63b9a80a5c11
Laelaf <laelaf.sirak@gmail.com>
2023-03-31 15:02:19 -0700

    before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,5 @@
-#include <stdio.h>
+#include "lists.h"
 
-void __attribute__((constructor)) before_main()
-{
+void before_main(void) {
     printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-}
-int main(void)
-{
-	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-	return (0);
 }
