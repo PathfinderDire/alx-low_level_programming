eb89734f5c3c6179ae6f75610d1af4ca2c1fb24c
Laelaf <laelaf.sirak@gmail.com>
2023-04-05 11:57:23 -0700

    add node at index odx

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (temp != NULL && p < idx - 1)
 	{
 		temp = temp->next;
-		i++;
+		p++;
 	}
 	if (temp == NULL)
 		return (NULL);
